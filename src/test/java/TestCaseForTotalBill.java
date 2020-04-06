8ce94788acc32741b9aad7ac7a1805e46b3ba8c7
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-06 20:39:49 +0530

    ADDED : Test cases for the total fare calculation

--- src/test/java/TestCaseForTotalBill.java
+++ src/test/java/TestCaseForTotalBill.java
@@ -1,4 +1,25 @@
-public class TestCaseForTotalBill
-{
+import com.BridgeLabzs.TotalBillGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCaseForTotalBill {
+    @Test
+    public void givenDistanceNTime_WhenProper_ShouldRtuenTotalFare() {
+        TotalBillGenerator totalBillGenerator = new TotalBillGenerator();
+        double kiloMeters = 15;
+        int time = 20;
+        double totalFare = totalBillGenerator.calculateFare(kiloMeters, time);
+        Assert.assertEquals(170, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenNigligibleDistanceNTime_ShouldReturnMinimumFare() {
+        TotalBillGenerator totalBillGenerator = new TotalBillGenerator();
+        double kiloMeters = 0.2;
+        int time = 2;
+        double totalFare = totalBillGenerator.calculateFare(kiloMeters, time);
+        Assert.assertEquals(5, totalFare, 0.0);
+
+    }
 
 }
