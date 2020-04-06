fbff91639961f699ff34fa9be6536e57ab54ee12
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-06 20:38:19 +0530

    ADDED : Method to calculate fare

--- src/main/java/com/BridgeLabzs/TotalBillGenerator.java
+++ src/main/java/com/BridgeLabzs/TotalBillGenerator.java
@@ -1,9 +1,18 @@
 package com.BridgeLabzs;
 
-public class TotalBillGenerator
-{
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator");
-    }
+public class TotalBillGenerator {
+    //CONSTANTS
+    public static final int COST_PER_KILOMETER = 10;
+    public static final int MINIMUM_FARE = 5;
+    public static final int COST_PER_MINUTE = 1;
 
+    public double calculateFare(double kiloMeters, int time) {
+        double totalFare = ((kiloMeters * COST_PER_KILOMETER) + (COST_PER_MINUTE * time));
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else
+            return totalFare;
+
+
+    }
 }
