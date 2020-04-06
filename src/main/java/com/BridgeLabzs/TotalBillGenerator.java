865aebee7f7d091ead1ef67f8b55a786fdc1cd07
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-06 11:44:50 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/BridgeLabzs/TotalBillGenerator.java
@@ -0,0 +1,9 @@
+package com.BridgeLabzs;
+
+public class TotalBillGenerator
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator");
+    }
+
+}
