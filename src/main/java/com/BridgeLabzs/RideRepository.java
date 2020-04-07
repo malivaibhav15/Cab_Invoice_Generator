75404b48b19ea90ad01d7018a6097c46aafdca62
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-07 20:29:20 +0530

    ADDED : Class for multiple ride

--- src/main/java/com/BridgeLabzs/RideRepository.java
+++ src/main/java/com/BridgeLabzs/RideRepository.java
@@ -1,4 +1,27 @@
 package com.BridgeLabzs;
 
-public class RideRepository {
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class RideRepository
+{
+    Map<String, ArrayList<MultipleRides>> userRides = null;
+
+    public RideRepository()
+    {
+        this.userRides = new HashMap<>();
+    }
+    public void addRides(String userName , MultipleRides[] multipleRides)
+    {
+        ArrayList<MultipleRides> rideList = this.userRides.get(userName);
+        if (rideList == null)
+        {
+           this.userRides.put(userName, new ArrayList<>(Arrays.asList(multipleRides)));
+        }
+
+    }
+
+    public MultipleRides[] getRides(String userName) {
+        return this.userRides.get(userName).toArray(new MultipleRides[0]);
+    }
 }
