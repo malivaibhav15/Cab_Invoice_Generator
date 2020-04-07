e7888f3b037c53ff7a5671f7fe9f7288464f326a
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-07 10:33:29 +0530

    ADDED : Test case for calculating the aggrigate of multiple rides

--- src/test/java/TestCaseForTotalBill.java
+++ src/test/java/TestCaseForTotalBill.java
@@ -1,11 +1,14 @@
+import com.BridgeLabzs.MultipleRIdes;
 import com.BridgeLabzs.TotalBillGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCaseForTotalBill {
+
+    TotalBillGenerator totalBillGenerator = new TotalBillGenerator();
+
     @Test
     public void givenDistanceNTime_WhenProper_ShouldRtuenTotalFare() {
-        TotalBillGenerator totalBillGenerator = new TotalBillGenerator();
         double kiloMeters = 15;
         int time = 20;
         double totalFare = totalBillGenerator.calculateFare(kiloMeters, time);
@@ -14,12 +17,17 @@ public class TestCaseForTotalBill {
 
     @Test
     public void givenNigligibleDistanceNTime_ShouldReturnMinimumFare() {
-        TotalBillGenerator totalBillGenerator = new TotalBillGenerator();
         double kiloMeters = 0.2;
         int time = 2;
         double totalFare = totalBillGenerator.calculateFare(kiloMeters, time);
         Assert.assertEquals(5, totalFare, 0.0);
-
     }
 
+    @Test
+    public void givenMultipleRides_WhenProper_ShouldReturnAggrigate() {
+        MultipleRIdes[] multipleRIdes = {new MultipleRIdes(10, 15),
+                new MultipleRIdes(0.2, 2)};
+        double totalFare = totalBillGenerator.calculateFare(multipleRIdes);
+        Assert.assertEquals(120, totalFare, 0.0);
+    }
 }
