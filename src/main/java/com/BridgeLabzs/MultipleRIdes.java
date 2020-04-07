008a132ed5c40be0b0623b14d926e6b697bf28b5
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-07 10:31:06 +0530

    ADDED : Class for multiple rides

---
+++ src/main/java/com/BridgeLabzs/MultipleRIdes.java
@@ -0,0 +1,11 @@
+package com.BridgeLabzs;
+
+public class MultipleRIdes {
+    public double kiloMeters;
+    public int time;
+
+    public MultipleRIdes(double kiloMeters, int time) {
+        this.kiloMeters = kiloMeters;
+        this.time = time;
+    }
+}
