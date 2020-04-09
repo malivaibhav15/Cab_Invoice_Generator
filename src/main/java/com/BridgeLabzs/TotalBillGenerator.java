465ba96af28ffa633453c3e51bd2f783910145f9
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-09 15:32:30 +0530

    ADDED : Variables and method for premium rides

--- src/main/java/com/BridgeLabzs/TotalBillGenerator.java
+++ src/main/java/com/BridgeLabzs/TotalBillGenerator.java
@@ -6,21 +6,36 @@ public class TotalBillGenerator {
     public static final int COST_PER_KILOMETER = 10;
     public static final int MINIMUM_FARE = 5;
     public static final int COST_PER_MINUTE = 1;
-    private RideRepository rideRepository;
+    private static final double PREMIUM_MINIMUM_COST_PER_KILOMETER = 15;
+    private static final int PREMIUM_COST_PER_MIN = 2;
+    private static final double PREMIUM_MINIMUM_FARE = 20;
 
-    public TotalBillGenerator() {
-        this.rideRepository = new RideRepository();
+    private TypesOfCabs types;
+    RideRepository rideRepository = new RideRepository();
+
+    public TotalBillGenerator(TypesOfCabs types) {
+        this.types = types;
     }
 
     //METHOD FOR CALCULATING THE TOTAL FARE
-    public double calculateFare(double kiloMeters, int time) {
-        double totalFare = ((kiloMeters * COST_PER_KILOMETER) + (COST_PER_MINUTE * time));
-        return totalFare < MINIMUM_FARE ? MINIMUM_FARE : totalFare;
+    public double calculateFare(double distance, int time) {
+        switch (this.types) {
+            case NORMAL_RIDES: {
+                double totalFare = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+                return Math.max(totalFare, MINIMUM_FARE);
+            }
+            case PREMIUM_RIDES: {
+                double totalFare = (distance * PREMIUM_MINIMUM_COST_PER_KILOMETER) + (time * PREMIUM_COST_PER_MIN);
+                return Math.max(totalFare, PREMIUM_MINIMUM_FARE);
+            }
+        }
+        return 0.0;
     }
 
     //METHOD FOR CALCULATING THE AGGRIGATE OF MULTIPLE RIDES
     public FareSummery calculateFare(MultipleRides[] multipleRides) {
         double totalFare = 0;
+        double[] result = new double[3];
         for (MultipleRides multipleRides1 : multipleRides) {
             totalFare = totalFare + this.calculateFare(multipleRides1.kiloMeters, multipleRides1.time);
         }
@@ -28,7 +43,7 @@ public class TotalBillGenerator {
     }
 
     public void calculateFare(String userName, MultipleRides[] multipleRides) {
-        rideRepository.addRides(userName,multipleRides);
+        rideRepository.calculateFare(userName,multipleRides);
     }
 
     public FareSummery getFareSummery(String userName) {
---
+++ src/main/java/com/BridgeLabzs/TypesOfCabs.java
@@ -0,0 +1,4 @@
+package com.BridgeLabzs;
+
+public enum TypesOfCabs {
+}
